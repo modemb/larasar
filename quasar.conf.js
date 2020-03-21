b71597ce63e1dbb1b83e38aad61c8f066bd93edf
Mohamed Dembele <modembfr@gmail.com>
2020-03-20 21:28:21 -0400

    Email Verification

--- quasar.conf.js
+++ quasar.conf.js
@@ -2,7 +2,7 @@
 // https://quasar.dev/quasar-cli/quasar-conf-js
 module.exports = function (ctx) {
   // https://quasar.dev/quasar-cli/quasar-conf-js#Example-setting-env-for-dev%2Fbuild
-  let localProdServer = 1 // WampServer
+  let localProdServer = 0 // WampServer
   return {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
@@ -74,7 +74,7 @@ module.exports = function (ctx) {
       // ================= \\
       // vueCompiler: true,
       // https://quasar.dev/app-extensions/discover#Introduction
-      env: localProdServer ? { LOCAL_PROD: 1 } : '',
+      // env: localProdServer ? { LOCAL_PROD: 1 } : '',
       distDir: 'public/quasar', // Comment for dist folder
       publicPath: localProdServer ? '/larasar/public' : '/',
 
